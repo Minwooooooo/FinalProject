cb3c90dde16c025a129d442123cabb57e2953331
Soyun Im <soyun012399@naver.com>
2022-09-27 18:33:57 +0900

    soyun commit

---
+++ src/main/java/com/example/demo/entity/Memo.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity;
+
+import com.example.demo.dto.requestDto.MemoRequestDto;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Memo extends Timestamped{
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn
+    private Member member;
+
+    @Column
+    private String contents;
+
+    @Column
+    private LocalDate createDate;
+
+    @Column
+    private LocalDate modifiedDate;
+
+    @Column
+    private String category;
+
+    @Column
+    private String roomName;
+
+
+    public void updateMemo(String contents) {
+        this.contents = contents;
+        this.modifiedDate = LocalDate.now();
+    }
+
+
+
+}
--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -4,4 +4,6 @@ import com.example.demo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByMemberName(String memberName);
 }
