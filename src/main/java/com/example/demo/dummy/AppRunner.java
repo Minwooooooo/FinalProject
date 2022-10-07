2dc8aa924ee57d149ccc6b44108e3db7a7912e34
Minwooooooo <achilleus_@nate.com>
2022-10-08 03:14:20 +0900

    Data Script 추가
    - MySQL 이전 후 AppRunner 초기화
    - 2,3인 Script 추가 및 해당 Controller+Service 생성

--- src/main/java/com/example/demo/dummy/AppRunner.java
+++ src/main/java/com/example/demo/dummy/AppRunner.java
@@ -21,35 +21,6 @@ public class AppRunner implements ApplicationRunner {
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("AppRunner 실행");
-
-        Script script;
-        script= Script.builder()
-                .type(2)
-                .category("#미드,#썸,#10대,#하이틴")
-                .story("- **A: hi.**\n" +
-                        "    - 안녕.\n" +
-                        "- **B: bye.**\n" +
-                        "    - 안녕.\n")
-                .build();
-        scriptRepository.save(script);
-        script=Script.builder()
-                .type(2)
-                .category("#예약,#병원,#전화,#진료예약")
-                .story("- **A: Dr. Willam’s Office.**\n" +
-                        "    - 닥터 윌리엄 병원입니다.\n" +
-                        "- **B: Yes.**\n" +
-                        "    - 네.\n")
-                .build();
-        scriptRepository.save(script);
-        script=Script.builder()
-                .type(2)
-                .category("#미드,#연인,#연애")
-                .story("- **A: You know,**\n" +
-                        "    - 있잖아,\n" +
-                        "- **B: Of course!**\n" +
-                        "    - 당연하지!?\n")
-                .build();
-        scriptRepository.save(script);
     }
 }
 
